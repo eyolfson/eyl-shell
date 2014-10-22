5d6c0d6d8d3a4954bd51713d15cd60c93fe7fe3f
Jon Eyolfson <jon@eyl.io>
2014-10-21 23:01:08 -0400

    Take homedir by reference instead of copying

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let logfile_path = match std::os::homedir() {
-        Some(p) => p.join(".eyl-shell.log"),
-        None    => fail!("finding homedir")
+        Some(ref p) => p.join(".eyl-shell.log"),
+        None        => fail!("finding homedir")
     };
     let mut logfile = std::io::File::create(&logfile_path);
     match logfile.write(b"It works!\n") {
