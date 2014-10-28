2c38ce28ff0c53046a6898ed6a668dee12eeb5cd
Jon Eyolfson <jon@eyl.io>
2014-10-28 11:41:32 -0400

    Clean up the raw module a bit

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod raw {
     extern crate libc;
 
+    use self::libc::{c_char, c_void};
+
     #[allow(non_camel_case_types)]
-    pub type wl_display = libc::c_void;
+    pub type wl_display = c_void;
 
     #[link(name = "wayland-client")]
     extern {
-        pub fn wl_display_connect(name: *const u8) -> *mut wl_display;
+        pub fn wl_display_connect(name: *const c_char) -> *mut wl_display;
         pub fn wl_display_disconnect(display: *mut wl_display);
     }
 }
