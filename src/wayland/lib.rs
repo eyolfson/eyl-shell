5d554b85f0afe90423ad0f5cc9e620c7945afac4
Jon Eyolfson <jon@eyl.io>
2014-10-28 21:57:44 -0400

    Added some Wayland list bindings

--- src/wayland/lib.rs
+++ src/wayland/lib.rs
@@ -1,5 +1,7 @@
 pub use display::Display;
+pub use list::List;
 
 mod display;
+mod list;
 
 pub mod raw;
--- src/wayland/raw/mod.rs
+++ src/wayland/raw/mod.rs
@@ -2,7 +2,13 @@
 
 extern crate libc;
 
-use self::libc::{c_char, c_void};
+use self::libc::{c_char, c_int, c_void};
+
+#[repr(C)]
+pub struct wl_list {
+    pub prev: *mut wl_list,
+    pub next: *mut wl_list,
+}
 
 pub type wl_display = c_void;
 
@@ -10,4 +16,11 @@ pub type wl_display = c_void;
 extern {
     pub fn wl_display_connect(name: *const c_char) -> *mut wl_display;
     pub fn wl_display_disconnect(display: *mut wl_display);
+
+    pub fn wl_list_init(list: *mut wl_list);
+    pub fn wl_list_insert(list: *mut wl_list, elm: *mut wl_list);
+    pub fn wl_list_remove(elm: *mut wl_list);
+    pub fn wl_list_length(elm: *const wl_list) -> c_int;
+    pub fn wl_list_empty(elm: *const wl_list) -> c_int;
+    pub fn wl_list_insert_list(list: *mut wl_list, other: *mut wl_list);
 }
