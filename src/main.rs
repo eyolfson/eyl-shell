e1636caa4a71da25105fdababe230a29b20e4170
Jon Eyolfson <jon@eyl.io>
2014-10-28 20:33:09 -0400

    I wrote some bad bad code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 extern crate wayland;
 
+static mut LOGFILE: std::io::fs::File = match std::os::homedir() {
+    Some(ref p) =>
+        match std::io::File::create(&p.join(".eyl-shell.log")) {
+            Ok(f) => f,
+            Err(_) => fail!("?")
+        },
+    None        => fail!("finding homedir")
+};
+
 fn main() {
     let logfile_path = match std::os::homedir() {
         Some(ref p) => p.join(".eyl-shell.log"),
