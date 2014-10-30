829e03fc9a21ac426806531c305ab87962b56049
Jon Eyolfson <jon@eyl.io>
2014-10-30 12:42:42 -0400

    Changed the formatting of multiline function declarations

--- src/wayland/raw/mod.rs
+++ src/wayland/raw/mod.rs
@@ -26,27 +26,34 @@ extern {
     pub fn wl_display_connect(name: *const c_char) -> *mut wl_display;
     pub fn wl_display_disconnect(display: *mut wl_display);
     pub fn wl_display_dispatch(display: *mut wl_display) -> c_int;
-    pub fn wl_display_dispatch_queue(display: *mut wl_display,
-                                     queue: *mut wl_event_queue) -> c_int;
-    pub fn wl_display_dispatch_queue_pending(display: *mut wl_display,
-                                             queue: *mut wl_event_queue)
-                                             -> c_int;
+    pub fn wl_display_dispatch_queue(
+        display: *mut wl_display,
+        queue: *mut wl_event_queue
+    ) -> c_int;
+    pub fn wl_display_dispatch_queue_pending(
+        display: *mut wl_display,
+        queue: *mut wl_event_queue
+    ) -> c_int;
     pub fn wl_display_dispatch_pending(display: *mut wl_display) -> c_int;
     pub fn wl_display_get_error(display: *mut wl_display) -> c_int;
     pub fn wl_display_flush(display: *mut wl_display) -> c_int;
-    pub fn wl_display_roundtrip_queue(display: *mut wl_display,
-                                      queue: *mut wl_event_queue) -> c_int;
+    pub fn wl_display_roundtrip_queue(
+        display: *mut wl_display,
+        queue: *mut wl_event_queue
+    ) -> c_int;
     pub fn wl_display_roundtrip(display: *mut wl_display) -> c_int;
     pub fn wl_proxy_add_listener(
         proxy: *mut wl_proxy,
         implementation: *mut extern fn(c_void) -> c_void,
-        data: *mut c_void) -> c_int;
+        data: *mut c_void
+    ) -> c_int;
     pub fn wl_proxy_destroy(proxy: *mut wl_proxy);
-    pub fn wl_proxy_marshal_constructor(proxy: *mut wl_proxy,
-                                        opcode: uint32_t,
-                                        interface: *const util::wl_interface,
-                                        ...) -> *mut wl_proxy;
-
+    pub fn wl_proxy_marshal_constructor(
+        proxy: *mut wl_proxy,
+        opcode: uint32_t,
+        interface: *const util::wl_interface,
+        ...
+    ) -> *mut wl_proxy;
     pub fn wl_list_init(list: *mut wl_list);
     pub fn wl_list_insert(list: *mut wl_list, elm: *mut wl_list);
     pub fn wl_list_remove(elm: *mut wl_list);
